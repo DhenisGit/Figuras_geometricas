489e30807927cb5f58dd53555cc872c396a80825
Dhenis <dhenis@mail.com>
2023-07-28 15:07:48 -0500

    Avanzando el tema de la herencia

--- app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
+++ app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.dhenis.polomorfismo.ejemplos.Circulo;
-import com.dhenis.polomorfismo.ejemplos.Cuadrado;
-import com.dhenis.polomorfismo.ejemplos.Rectangulo;
+import com.dhenis.polomorfismo.herencia.Hijo;
+import com.dhenis.polomorfismo.herencia.Madre;
+import com.dhenis.polomorfismo.herencia.Padre;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,54 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*===================CIRCULO====================================*/
-        Circulo circulo = new Circulo(32, 3.14);
-        System.out.println("Radio del circulo: " + circulo.getRadio());
-        System.out.println("Valor del PI : " + circulo.getPi());
-        Double pi = circulo.getPi();
-        Integer radio = circulo.getRadio();
-        Integer radio_cuadrado = radio * radio;
-        Double resultado = radio_cuadrado * pi;
-        System.out.println("El area del circulo es: " + resultado);
-        /*=====================CUADRADO==================================*/
-        Cuadrado cuadrado = new Cuadrado(20, 150);
-        Integer lado1 = cuadrado.getLado1();
-        Integer lado2 = cuadrado.getLado2();
-        System.out.println("=======================");
-        System.out.println("Lado1: " + lado1);
-        System.out.println("Lado2: " + lado2);
-        System.out.println("=======================");
-        Integer resultado1 = lado1 * lado2;
-        System.out.println("=================================");
-        System.out.println("El resultado es: " + resultado1);
-        System.out.println("=================================");
-        /*=============================================================*/
-        cuadrado.setLado1(5);
-        cuadrado.setLado2(15);
-        System.out.println("=======================");
-        Integer primer_lado = cuadrado.getLado1();
-        Integer segundo_lado = cuadrado.getLado2();
-        System.out.println("Lado1: " + primer_lado);
-        System.out.println("Lado2: " + segundo_lado);
-        System.out.println("=======================");
-        Integer resultado2 = primer_lado * segundo_lado;
-        System.out.println("=================================");
-        System.out.println("El resultado es: " + resultado2);
-        System.out.println("=================================");
-        /*=============================================================*/
-        cuadrado.setLado1(10);
-        cuadrado.setLado2(10);
-        System.out.println("=======================");
-        Integer lado_1 = cuadrado.getLado1();
-        Integer lado_2 = cuadrado.getLado2();
-        System.out.println("Lado1: " + lado_1);
-        System.out.println("Lado2: " + lado_2);
-        Integer resultado3 = lado_1 * lado_2;
-        System.out.println("=================================");
-        System.out.println("El resultado es: " + resultado3);
-        System.out.println("=================================");
-        /*========================RECTANGULO===============================*/
-        Rectangulo rectangulo = new Rectangulo(10, 8);
-        rectangulo.calcularArea();
+
+        List<Padre> padreList = new ArrayList<>();
+        padreList.add(new Padre(0, "German", "Gonzalo", "Quispe", 39));
+        padreList.add(new Padre(2, "Gerardo", "Valencia", "Quiños", 41));
+        padreList.add(new Padre(3, "Alejandro", "Perez", "Garcia", 33));
+        padreList.add(new Padre(4, "Diego", "Martines", "Osorio", 29));
+        padreList.add(new Padre(5, "Marcos", "Calderon", "Peralta", 36));
+
+        List<Madre> madresList = new ArrayList<>();
+        madresList.add(new Madre(02, "Maria", "Gomez", "Fernandez", 29));
+        madresList.add(new Madre(2, "Ana", "Rodriguez", "Soto", 39));
+        madresList.add(new Madre(3, "Laura", "Ramirez", "Morales", 22));
+        madresList.add(new Madre(4, "Sofia", "Lopez", "Silva", 26));
+        madresList.add(new Madre(5, "Isabel", "Sanchez", "Ortega", 28));
+
+        int id = 0;
+        id = padreList.get(id).getId();
+        List<Hijo> hijosList = new ArrayList<>();
+        hijosList.add(new Hijo(1, "Dhenis", padreList.get(id).getApellido_p(), "a", 20));
+        /*hijosList.add(new Hijo(2, "Andres", padreList.get(1).getApellido_p(), madresList.get(1).getApellido_p(), 10));
+        hijosList.add(new Hijo(3, "Cristiano", padreList.get(2).getApellido_p(), madresList.get(2).getApellido_p(), 6));
+        hijosList.add(new Hijo(4, "Messi", padreList.get(3).getApellido_p(), madresList.get(3).getApellido_p(), 12));
+        hijosList.add(new Hijo(5, "Neymar", padreList.get(4).getApellido_p(), madresList.get(4).getApellido_p(), 23));*/
+        System.out.println("==========================================");
+        System.out.println("Apellido paterno: " + hijosList.get(id).getApellido_p());
+        System.out.println("==========================================");
+
+
     }
 }
---
+++ app/src/main/java/com/dhenis/polomorfismo/herencia/Hijo.java
@@ -0,0 +1,7 @@
+package com.dhenis.polomorfismo.herencia;
+
+public class Hijo extends Padre {
+    public Hijo(Integer id, String nombre, String apellido_p, String apellido_m, Integer edad) {
+        super(id, nombre, apellido_p, apellido_m, edad);
+    }
+}
