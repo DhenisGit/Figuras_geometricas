fab874a6f3dfadeb796c32f7aaedd0b06d125a32
Dhenis <dhenis@mail.com>
2023-07-28 22:10:42 -0500

    Herencia aplicada correctamente con listas

--- app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
+++ app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
@@ -20,30 +20,32 @@ public class MainActivity extends AppCompatActivity {
 
         List<Padre> padreList = new ArrayList<>();
         padreList.add(new Padre(0, "German", "Gonzalo", "Quispe", 39));
-        padreList.add(new Padre(2, "Gerardo", "Valencia", "Quiños", 41));
-        padreList.add(new Padre(3, "Alejandro", "Perez", "Garcia", 33));
-        padreList.add(new Padre(4, "Diego", "Martines", "Osorio", 29));
-        padreList.add(new Padre(5, "Marcos", "Calderon", "Peralta", 36));
+        padreList.add(new Padre(1, "Gerardo", "Valencia", "Quiños", 41));
+        padreList.add(new Padre(2, "Alejandro", "Perez", "Garcia", 33));
+        padreList.add(new Padre(3, "Diego", "Martines", "Osorio", 29));
+        padreList.add(new Padre(4, "Marcos", "Calderon", "Peralta", 36));
 
         List<Madre> madresList = new ArrayList<>();
-        madresList.add(new Madre(02, "Maria", "Gomez", "Fernandez", 29));
-        madresList.add(new Madre(2, "Ana", "Rodriguez", "Soto", 39));
-        madresList.add(new Madre(3, "Laura", "Ramirez", "Morales", 22));
-        madresList.add(new Madre(4, "Sofia", "Lopez", "Silva", 26));
-        madresList.add(new Madre(5, "Isabel", "Sanchez", "Ortega", 28));
+        madresList.add(new Madre(0, "Maria", "Gomez", "Fernandez", 29));
+        madresList.add(new Madre(1, "Ana", "Rodriguez", "Soto", 39));
+        madresList.add(new Madre(2, "Laura", "Ramirez", "Morales", 22));
+        madresList.add(new Madre(3, "Sofia", "Lopez", "Silva", 26));
+        madresList.add(new Madre(4, "Isabel", "Sanchez", "Ortega", 28));
 
-        int id = 0;
+        int id = 1;
         id = padreList.get(id).getId();
         List<Hijo> hijosList = new ArrayList<>();
-        hijosList.add(new Hijo(1, "Dhenis", padreList.get(id).getApellido_p(), "a", 20));
-        /*hijosList.add(new Hijo(2, "Andres", padreList.get(1).getApellido_p(), madresList.get(1).getApellido_p(), 10));
-        hijosList.add(new Hijo(3, "Cristiano", padreList.get(2).getApellido_p(), madresList.get(2).getApellido_p(), 6));
-        hijosList.add(new Hijo(4, "Messi", padreList.get(3).getApellido_p(), madresList.get(3).getApellido_p(), 12));
-        hijosList.add(new Hijo(5, "Neymar", padreList.get(4).getApellido_p(), madresList.get(4).getApellido_p(), 23));*/
+        hijosList.add(new Hijo(0, "Dhenis", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 20));
+        hijosList.add(new Hijo(1, "Andres", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 9));
+        hijosList.add(new Hijo(2, "Cristiano", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 2));
+        hijosList.add(new Hijo(3, "Messi", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 21));
+        hijosList.add(new Hijo(4, "Neymar", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 12));
         System.out.println("==========================================");
+        System.out.println("ID del hijo: " + hijosList.get(id).getId());
+        System.out.println("Nombre del hijo: " + hijosList.get(id).getNombre());
         System.out.println("Apellido paterno: " + hijosList.get(id).getApellido_p());
+        System.out.println("Apellido materno: " + hijosList.get(id).getApellido_m());
+        System.out.println("Edad del hijo: " + hijosList.get(id).getEdad());
         System.out.println("==========================================");
-
-
     }
 }
