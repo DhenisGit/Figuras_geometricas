0bb7ae0a72f39ee5528aedb292bf775afbafe3dd
Dhenis <dhenis@mail.com>
2023-08-01 14:42:10 -0500

    Imprimiendo la cantidad de hijos referente a los padres ejemplos(Los padres pueden tener 2 o mas hijos(as))

--- app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
+++ app/src/main/java/com/dhenis/polomorfismo/MainActivity.java
@@ -19,33 +19,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         List<Padre> padreList = new ArrayList<>();
-        padreList.add(new Padre(0, "German", "Gonzalo", "Quispe", 39));
+        padreList.add(new Padre(5, "German", "Gonzalo", "Quispe", 39));
         padreList.add(new Padre(1, "Gerardo", "Valencia", "Quiños", 41));
         padreList.add(new Padre(2, "Alejandro", "Perez", "Garcia", 33));
-        padreList.add(new Padre(3, "Diego", "Martines", "Osorio", 29));
+        padreList.add(new Padre(3, "Diego", "Martinez", "Osorio", 29));
         padreList.add(new Padre(4, "Marcos", "Calderon", "Peralta", 36));
 
         List<Madre> madresList = new ArrayList<>();
-        madresList.add(new Madre(0, "Maria", "Gomez", "Fernandez", 29));
+        madresList.add(new Madre(5, "Maria", "Gomez", "Fernandez", 29));
         madresList.add(new Madre(1, "Ana", "Rodriguez", "Soto", 39));
         madresList.add(new Madre(2, "Laura", "Ramirez", "Morales", 22));
         madresList.add(new Madre(3, "Sofia", "Lopez", "Silva", 26));
         madresList.add(new Madre(4, "Isabel", "Sanchez", "Ortega", 28));
 
-        int id = 1;
-        id = padreList.get(id).getId();
+        int id = 5;
         List<Hijo> hijosList = new ArrayList<>();
-        hijosList.add(new Hijo(0, "Dhenis", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 20));
+        hijosList.add(new Hijo(5, "Dhenis", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 20));
         hijosList.add(new Hijo(1, "Andres", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 9));
+        hijosList.add(new Hijo(5, "Dhenis2", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 10));
         hijosList.add(new Hijo(2, "Cristiano", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 2));
         hijosList.add(new Hijo(3, "Messi", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 21));
         hijosList.add(new Hijo(4, "Neymar", padreList.get(id).getApellido_p(), madresList.get(id).getApellido_p(), 12));
-        System.out.println("==========================================");
-        System.out.println("ID del hijo: " + hijosList.get(id).getId());
-        System.out.println("Nombre del hijo: " + hijosList.get(id).getNombre());
-        System.out.println("Apellido paterno: " + hijosList.get(id).getApellido_p());
-        System.out.println("Apellido materno: " + hijosList.get(id).getApellido_m());
-        System.out.println("Edad del hijo: " + hijosList.get(id).getEdad());
-        System.out.println("==========================================");
+
+        List<Hijo> hijosConMismoId = new ArrayList<>();
+        for (Hijo hijo : hijosList) {
+            if (hijo.getId() == id) {
+                hijosConMismoId.add(hijo);
+            }
+        }
+        for (Hijo hijo : hijosConMismoId) {
+            System.out.println("=================PADRES===================");
+            System.out.println("ID del hijo: " + hijo.getId());
+            System.out.println("Nombre del hijo: " + hijo.getNombre());
+            System.out.println("=================CANT_HIJOS===================");
+            System.out.println("ID del hijo: " + hijo.getId());
+            System.out.println("Nombre del hijo: " + hijo.getNombre());
+            System.out.println("Apellido paterno: " + hijo.getApellido_p());
+            System.out.println("Apellido materno: " + hijo.getApellido_m());
+            System.out.println("Edad del hijo: " + hijo.getEdad());
+            System.out.println("==========================================");
+        }
     }
 }
