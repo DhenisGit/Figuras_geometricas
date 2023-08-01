c80791ea6c46d377c955b1c869568e0e168bf0e5
yoel <yoelvasuqezvasquez2021@gmail.com>
2023-08-01 22:04:56 +0000

    subido

---
+++ app/src/main/java/com/dhenis/polomorfismo/listapadre.java
@@ -0,0 +1,14 @@
+package com.dhenis.polomorfismo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listapadre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listapadre);
+    }
+}
